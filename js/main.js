434eddc658e3359560f228099aa883a034737b19
Matteo Susanna <matteo2784@gmail.com>
2022-07-21 16:03:33 +0200

    Aggiunta

--- js/main.js
+++ js/main.js
@@ -2,8 +2,19 @@ var app = new Vue({
     el: '#app',
     data: {
       message: 'BENVENUTO IN Vue.js',
-      linkImg : 'https://avatars.githubusercontent.com/u/6128107?s=200&v=4'
+      linkImg : 'https://avatars.githubusercontent.com/u/6128107?s=200&v=4',
+      myClass : 'color-red'
+    },
+
+    methods: {
+      color (){
+        if(this.myClass == 'color-red'){
+          this.myClass = 'color-orange'
+        }else{
+          this.myClass = 'color-red'
+        }
+      }
+      
     }
-    
   })
 
